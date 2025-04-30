09a83117df51344f32e46b09c3f0f718f7879241
Zack Goldblum <zackgoldblum@gmail.com>
2025-04-30 10:36:26 -0400

    Revert "Deploy to prod"

--- terraform/cloudwatch.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "goldblum_askeeg" {
-  name              = "/ecs/goldblum-askeeg"
-  retention_in_days = 30
-
-  tags = {
-    Name  = "goldblum-askeeg"
-    Owner = element(split("/", data.aws_caller_identity.current.arn), 1)
-  }
-} 
